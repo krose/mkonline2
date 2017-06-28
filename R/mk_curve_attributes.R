a9ac56350dc5172e3dff3a154889ff1ed55b8773
Kenneth Rose <kennethrose82@gmail.com>
2017-06-28 23:13:22 +0200

    Function to get the curve_attributes.

---
+++ R/mk_curve_attributes.R
@@ -0,0 +1,60 @@
+#' FUnction to get the curve attributes.
+#'
+#' @param curve_attr The name of the curve attribute you want. The
+#'     current possibilities are: areas, categories, commodities
+mk_curve_attributes <- function(curve_attr, token = NULL){
+
+  ######################################
+  # Checks
+  # - Check possible curve attr
+  # - check number of attr
+  # - check if token is supplied.
+  ######################################
+
+  poss_curve_attr <- c("areas", "categories", "commodities",
+                       "curve_types", "data_types", "frequencies",
+                       "functions", "sources", "stations", "storage_types",
+                       "time_zones", "units")
+
+  if(!curve_attr %in% poss_curve_attr){
+    stop(paste0("The curve_attr '", curve_attr, "' is not one of the possible attributes."))
+  }
+
+  if(length(curve_attr) > 1){
+    stop("You can only supply one curve_attr to the mk_curve_attributes function.")
+  }
+
+  # If there is no token, get one
+  if(is.null(token)){
+    token <- mk_token()
+  }
+
+  #######################################
+  # Get the data
+  # - construct url
+  # - make the get request
+  # - Check for errors.
+  # - extract the actual content
+  ########################################
+
+  # Construct the url
+  url <- paste0("https://data.mkonline.io/api/", curve_attr)
+
+  cnt <- httr::GET(url,
+                   httr::user_agent("https://github.com/krose/mkonline2"),
+                   httr::add_headers('Authorization' = paste("Bearer", token$access_token)))
+
+  # Return an error if is one.
+  if(httr::http_error(cnt)){
+    stop(paste0("The attributes for ",
+                curve_attr,
+                " could not be retrieved. Error message: ",
+                httr::http_status(cnt)$message))
+  }
+
+  # Extract the content of the request.
+  cnt <- httr::content(x = cnt, as = "text", encoding = "UTF-8")
+  cnt <- jsonlite::fromJSON(cnt)
+
+  cnt
+}
